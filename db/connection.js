e1f7597fdf0499178c132d47118d072ddc0cae36
Mital <mitalg0717@gmail.com>
2021-11-11 18:58:34 -0500

    update code

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'Mital@80',
+    password: '',
     database: 'election'
   });
 
